df5ca46a56dd948eec1c627650a2cdd2fdc618d9
NataBoginia <nata_boginia@mail.ru>
2019-04-18 19:29:03 +0300

    Start correct styles for mobile index page

---
+++ source/js/app.js
@@ -0,0 +1,16 @@
+var menuToggle = document.querySelector(".main-menu__toggle");
+var menuList = document.querySelector(".main-menu__list");
+
+menuToggle.addEventListener("click", function (evt) {
+  if (menuToggle.classList.contains("main-menu__toggle--opened")) {
+    menuToggle.classList.remove("main-menu__toggle--opened");
+    menuToggle.classList.add("main-menu__toggle--closed");
+    menuList.classList.remove("main-menu__list--opened");
+    menuList.classList.add("main-menu__list--closed");
+  } else {
+    menuToggle.classList.remove("main-menu__toggle--closed");
+    menuToggle.classList.add("main-menu__toggle--opened");
+    menuList.classList.remove("main-menu__list--closed");
+    menuList.classList.add("main-menu__list--opened");
+  }
+});
