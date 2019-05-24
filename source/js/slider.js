a943fee0583afdf2969525943133163aa9d148f5
NataBoginia <nata_boginia@mail.ru>
2019-05-25 01:45:43 +0300

    Fix tablet view

--- source/js/slider.js
+++ source/js/slider.js
@@ -16,9 +16,3 @@ window.onresize = function(event) {
     slider.value=0;
   };
 }
-
-slider.addEventListener("change", function (evt) {
-  if (window.matchMedia("(min-width: 768px)").matches && window.matchMedia("(max-width: 1299px)").matches) {
-    sliderContainerTablet.style.backgroundImage = "linear-gradient(#ffffff 169px, transparent 169px), linear-gradient(to right, #f2f2f2 " + slider.value + "%, #ebebeb " + slider.value + "%)";
-  };
-});
