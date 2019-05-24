871888d4254259829805cae4a754b12436b1cc78
NataBoginia <nata_boginia@mail.ru>
2019-05-24 17:45:59 +0300

    Minify html and js, preload fonts

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,9 @@ var rename = require("gulp-rename");
 var imagemin = require('gulp-imagemin');
 var webp = require('gulp-webp');
 var svgstore = require('gulp-svgstore');
+var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -56,9 +59,7 @@ gulp.task("refresh", function (done) {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**/*.{png,jpg,svg}",
-    "source/js/**",
-    "source/*.html"
+    "source/img/**/*.{png,jpg,svg}"
   ], {
     base: "source"
   })
@@ -97,7 +98,20 @@ gulp.task("webp", function () {
     .pipe(gulp.dest("build/img"));
 });
 
-// gulp.task("build", gulp.series("clean", "copy", "css", "optim", "webp", "sprite"));
-gulp.task("build", gulp.series("clean", "copy", "css", "webp", "sprite"));
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+});
 
+gulp.task("js", function () {
+  return pipeline(
+        gulp.src("source/js/*.js"),
+        uglify(),
+        gulp.dest("build/js")
+  );
+});
+
+// gulp.task("build", gulp.series("clean", "copy", "css", "optim", "webp", "sprite"));
+gulp.task("build", gulp.series("clean", "copy", "css", "html", "js", "webp", "sprite"));
 gulp.task("start", gulp.series("build", "server"));
